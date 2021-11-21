d2a3273dc9e9e6ae094e9e848f04025fac8abf8a
Juansecu <juansecoder@hotmail.com>
2021-11-20 19:38:50 -0500

    Initial commit

---
+++ src/test/java/com/juansecu/opentoonix/OpenToonixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juansecu.opentoonix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenToonixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
